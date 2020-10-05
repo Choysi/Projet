6e12199ed08411cdcd7e3609b41c4f4a52e850ec
Choysi <junco.valentin@gmail.com>
2020-10-05 15:34:31 +0200

    push 05/10 1

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Cette adresse mail est déjà utilisée")
  */
 class User implements UserInterface
 {
